1fcf24b070d66ce31a459d9ed2c10611563e68b5
NEW GITHUB NAME <aftab.sayed.0044@gmail.com>
2026-08-24 23:04:18 -0700

    Modify

--- name.py
+++ name.py
@@ -1,7 +1,7 @@
 first_name = input("Enter your first name: ")
 surname = input("Enter your surname: ")
-contact_number = input("Enter your contact number: ")
+
 
 print("First Name:", first_name)
 print("Surname:", surname)
-print("Contact Number:", contact_number)
+print("NEW LINE")
