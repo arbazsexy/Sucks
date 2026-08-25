86f909178c2278dac29af2400e71c976d9233c86
Arbaz345 <arbaz.s.sayed0022@gmail.com>
2026-08-24 23:06:14 -0700

    Update name.py

--- name.py
+++ name.py
@@ -1,6 +1,6 @@
 first_name = input("Enter your first name: ")
 surname = input("Enter your surname: ")
-
+city = input("Enter your City:")
 
 print("First Name:", first_name)
 print("Surname:", surname)
